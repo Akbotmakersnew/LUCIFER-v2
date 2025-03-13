69a419ae18b44cd543d684a526e34ef8445df841
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2025-03-13 21:48:23 +0530

    Update error_handler.py

--- error_handler.py
+++ error_handler.py
@@ -1,5 +1,16 @@
 from pyrogram import errors
 
+async def handle_peer_id_error(client, reqstr1):
+    try:
+        reqstr = await client.get_users(reqstr1)
+        return reqstr
+    except ValueError as e:
+        if "Peer id invalid" in str(e):
+            print("Error: Invalid peer ID.")
+        else:
+            print(f"Error: {e}")
+        return None
+        
 def handle_forbidden_error(app, chat_id, photo):
     try:
         app.send_photo(chat_id, photo)
