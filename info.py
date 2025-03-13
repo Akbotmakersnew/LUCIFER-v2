72f688dcd01ec531534966e3e9a6d1f25b43dde5
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2025-03-13 14:27:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,9 +22,9 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://graph.org/file/0200ac4ddac95e5de1f99-57ce81707fd739b33d.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "")
+NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/0200ac4ddac95e5de1f99-57ce81707fd739b33d.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/0200ac4ddac95e5de1f99-57ce81707fd739b33d.jpg")
-SPELL_IMG = environ.get("SPELL_IMG", "")
+SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/0200ac4ddac95e5de1f99-57ce81707fd739b33d.jpg")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
